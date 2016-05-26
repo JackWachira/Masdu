f9ef169051bc0eb84aee2cfab05104b9cc250b3f
“JackMwangi” <jackmwa94@gmail.com>
2016-05-26 13:02:10 +0300

    (Feature) Add list,destroy, update endpoints with decorator

--- Bucketlist_backend/bucket/permissions.py
+++ Bucketlist_backend/bucket/permissions.py
@@ -7,10 +7,5 @@ class IsOwner(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         # Write permissions are only allowed to the owner of the bucketlist.
-        return obj.owner == request.user
+        return obj.user == request.user
--- Bucketlist_backend/bucket/views.py
+++ Bucketlist_backend/bucket/views.py
@@ -6,15 +6,32 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from django.db import IntegrityError
 from rest_framework import status
+from bucket.permissions import IsOwner
+from django.utils.six import BytesIO
+from rest_framework.parsers import JSONParser
+from django.core import serializers
+from functools import wraps
 
 
-class BucketListView(viewsets.ViewSet):
+def isexisting(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        pk = kwargs.get('pk', "")
+        try:
+            bucketlist = BucketList.objects.get(pk=pk)
+            return f(*args, **kwargs)
+        except BucketList.DoesNotExist:
+            return Response({'error': 'The bucketlist does not exist'}, 404)
+    return decorated
+
+
+class BucketListView(viewsets.ModelViewSet):
     """
     Bucketlist view
     """
     queryset = BucketList.objects.all()
     serializer_class = BucketlistSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def create(self, request):
         serializer = BucketlistSerializer(data=request.data)
@@ -30,3 +47,26 @@ class BucketListView(viewsets.ViewSet):
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request):
+        bucketlists = BucketList.objects.all()
+        page = self.paginate_queryset(bucketlists)
+        if page:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(bucketlists, many=True)
+        return Response(serializer.data)
+
+    @isexisting
+    def destroy(self, request, pk=None):
+        bucketlist = BucketList.objects.get(pk=pk).delete()
+        return Response({'Message': 'Bucketlist deleted successfully'}, 200)
+
+    @isexisting
+    def update(self, request, pk=None):
+        serializer = BucketlistSerializer(
+            BucketList.objects.get(pk=pk), data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, 200)
+        return Response(serializer.errors, 400)
